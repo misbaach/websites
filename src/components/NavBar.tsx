f8fc81e323245dcf0fb42a12f9f3222c0364d456
Jaguar Deva Nanggalasakti Oktavian <jaguardeva@gmail.com>
2024-09-26 08:04:00 +0700

    navbar: add navbar and mapping nav items

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,20 +1,31 @@
 import Logo from "@/assets/smilearn_full_logo.png";
 import { Link } from "react-router-dom";
 import LangDropdown from "./LanguageDropdown";
+import MENU_LIST from "@/utils/MENU_LIST";
+import { useLocation } from "react-router-dom";
 
 export default function NavBar() {
+  const { pathname } = useLocation();
+
   return (
     <div className="flex h-[80px] w-full items-center justify-center shadow-lg">
       <nav className="mx-auto flex w-full max-w-[1240px] items-center justify-between">
         <div>
           <img src={Logo} alt="semilearn logo" className="h-[40px]" />
         </div>
-        <ul className="flex items-center">
-          <li>
-            <Link to="/" className="">
-              Home
-            </Link>
-          </li>
+        <ul className="flex items-center gap-3">
+          {MENU_LIST.map((item, index) => {
+            return (
+              <li key={index} className="">
+                <Link
+                  to={item.path}
+                  className={`rounded-md px-3 py-2 text-lg font-semibold hover:bg-gray-100 ${pathname === item.path ? "learn-color" : "text-gray-900"}`}
+                >
+                  {item.name}
+                </Link>
+              </li>
+            );
+          })}
         </ul>
         <LangDropdown />
         <div></div>
--- src/utils/MENU_LIST.ts
+++ src/utils/MENU_LIST.ts
@@ -3,6 +3,22 @@ const MENU_LIST = [
     name: "Home",
     path: "/",
   },
+  {
+    name: "About",
+    path: "/about",
+  },
+  {
+    name: "Quiz",
+    path: "/quiz",
+  },
+  {
+    name: "Learning Path",
+    path: "/learning-paths",
+  },
+  {
+    name: "Contact Us",
+    path: "/contact-us",
+  },
 ];
 
 export default MENU_LIST;
